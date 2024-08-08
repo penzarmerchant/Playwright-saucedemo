a89ce82ad2d66da72c22a7a3f3ace4b6a94281f4
penzar33 <penzarmerchant28@gmail.com>
2024-08-08 19:27:30 +0530

    Allure Report Integration

--- tests/login/TC_002.spec.ts
+++ tests/login/TC_002.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '../../fixtures/pomFixture';
 import * as saucedemoData from '../TestData/sauceDemoCredentials.json'
 
-    test('Login with valid username', async ({ page,loginPage}) => {
+    test('Login with invalid Password ', async ({ page,loginPage}) => {
     await page.goto('https://www.saucedemo.com/');
     await loginPage.enterUsername(saucedemoData.validUsername);
     await loginPage.enterPassword(saucedemoData.invalidPassword);
